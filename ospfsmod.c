9399582fae22a3356f2b741368a8e279b5875a4d
Garima Lunawat <g.me.luna@gmail.com>
2016-02-22 01:51:14 -0800

    indir2. indir, and direct implemented

--- ospfsmod.c
+++ ospfsmod.c
@@ -660,6 +660,10 @@ static int32_t
 indir2_index(uint32_t b)
 {
 	// Your code here.
+	if (b < OSPFS_NDIRECT + OSPFS_NINDIRECT)
+	{
+		return 0;
+	}
 	return -1;
 }
 
@@ -679,11 +683,19 @@ static int32_t
 indir_index(uint32_t b)
 {
 	// Your code here.
-	return -1;
+	if (b < OSPFS_NDIRECT)
+	{
+		return -1;
+	}
+	if (indir2_index(b) == -1)
+	{
+		return 0;
+	}
+	return (b - (OSPFS_NDIRECT + OSPFS_NINDIRECT) / OSPFS_NINDIRECT); //TACO check
 }
 
 
-// int32_t indir_index(uint32_t b)
+// int32_t direct_index(uint32_t b)
 //	Returns the indirect block index for file block b.
 //
 // Inputs:  b -- the zero-based index of the file block
@@ -696,7 +708,15 @@ static int32_t
 direct_index(uint32_t b)
 {
 	// Your code here.
-	return -1;
+	if (indir_index(b) == -1)
+	{
+		return b;
+	}
+	if (indir2_index(b) == 0)
+	{
+		return b - OSPFS_NDIRECT;
+	}
+	return ((b - OSPFS_NDIRECT) % OSPFS_NINDIRECT);  //TACO check
 }
 
 
